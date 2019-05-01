5062c3afadeb8ce84b9ffb4fb3262c246e73654f
Fabiano Souza <fabianohensouza@gmail.com>
2019-05-01 11:46:47 -0300

    Deleting removed files and folders

--- Aulas JavaScript/my_script.js
+++
@@ -1,9 +0,0 @@
-// Exibe um alerta na tela do Browser (Comentário de uma linha)
-alert('Olá, seja bem vindo ao curso!!!')
-
-/*
-Seleciona um objeto pelo ID
-e altera o valor deste objeto
-(Comentário de múltiplas linhas)
-*/
-document.getElementById('nome').value = 'Oi'
