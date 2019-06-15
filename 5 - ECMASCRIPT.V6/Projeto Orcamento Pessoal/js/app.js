da06cb3e6007794e80605e95c28af40d131f9bbe
Fabiano Souza <fabianohensouza@gmail.com>
2019-06-15 16:35:11 -0300

    Comminting the classes maded today!

---
+++ 5 - ECMASCRIPT.V6/Projeto Orcamento Pessoal/js/app.js
@@ -0,0 +1,26 @@
+class Expense {
+    constructor(year, month, day, type, description, value) {
+        this.year = year
+        this.month = month
+        this.day = day
+        this.type = type
+        this.description = description
+        this.value = value
+    }
+}
+
+function recordExpense() {
+    let year = document.getElementById('year')
+    let month = document.getElementById('month')
+    let day = document.getElementById('day')
+    let type = document.getElementById('type')
+    let description = document.getElementById('description')
+    let value = document.getElementById('value')
+
+    let expense = new Expense(year.value, month.value, day.value, type.value, description.value, value.value)
+    record(expense)
+}
+
+function record(data) {
+    localStorage.setItem('Expense', JSON.stringify(data))
+}
