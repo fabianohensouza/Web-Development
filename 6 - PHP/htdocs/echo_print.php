b8a0765f023aee34daf6020230fe118edb16c648
Fabiano Souza <fabianohensouza@gmail.com>
2019-07-27 11:19:17 -0300

    First classes about PHP 7 realizado em: Sat Jul 27

---
+++ 6 - PHP/htdocs/echo_print.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Curso PHP</title>
+    </head>
+
+    <body>
+
+        <?php 
+            echo 'Comando echo';
+            print ('Comando print<br>');
+            print 'Comando print';
+            echo print ('Comando print<br>');
+        ?>
+
+    </body>
+<html>
