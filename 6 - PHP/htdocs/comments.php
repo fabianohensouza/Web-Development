5b3a3340ffa836f3d10ccce28aa5d24a63a8c124
Fabiano Souza <fabianohensouza@gmail.com>
2019-07-28 12:09:56 -0300

    realizado em: Sun Jul 28

---
+++ 6 - PHP/htdocs/comments.php
@@ -0,0 +1,22 @@
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Curso PHP</title>
+    </head>
+
+    <body>
+
+        <?php 
+            //1 line comments
+             
+            ##1 line comments shell style
+             
+            /*
+                Block comments
+                in mutiples lines
+            */
+            
+        ?>
+
+    </body>
+<html>
