0a2194bd9cc496f591da3b009cf4f9c344e47247
Fabiano Souza <fabianohensouza@gmail.com>
2019-09-17 23:10:23 -0300

    Created the script send mail in PHP realized at: Tue Sep 17

--- 6 - PHP/htdocs/Projeto_SendMail/send_process.php
+++ 6 - PHP/htdocs/Projeto_SendMail/send_process.php
@@ -1,5 +1,37 @@
 <?php
 
-    print_r($_POST);
+    class Message {
+        private $to = null;
+        private $subject = null;
+        private $message = null;
 
+        public function __get($attr) {
+            return $this->$attr;
+        } 
+
+        public function __set($attr, $value) {
+            $this->$attr = $value;
+        } 
+
+        public function validateMessage() {
+            if(empty($this->to) || empty($this->subject) || empty($this->message)) {
+                return false;
+            }
+            return true;
+        } 
+    }
+
+    $message = new Message();
+
+    $message->__set('to', $_POST['to']);
+    $message->__set('subject', $_POST['subject']);
+    $message->__set('message', $_POST['message']);
+
+    if($message->validateMessage()) {
+        echo 'Mensagem válida.';
+    } else {
+        
+        echo 'Mensagem inválida.';
+    }
+    
 ?>
