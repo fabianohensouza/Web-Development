8f129abae7a80fd43912bb2a613365e249fae0a4
Fabiano Souza <fabianohensouza@gmail.com>
2019-11-11 22:53:09 -0300

    Finalizing the PDO classes, now I'll start the PDO App realized at: Mon Nov 11

--- 9 - PHP com PDO/sql_injection.php
+++ 9 - PHP com PDO/sql_injection.php
@@ -13,10 +13,24 @@
 
             //Creating the QUERY
             $query = "select * from tb_users where";
-            $query .= " email = '{$_POST['email']}'";
-            $query .= " AND password = '{$_POST['password']}'";
+            $query .= " email = :email";
+            $query .= " AND password = :password";
 
-            echo $query;
+            //Preparing the query to be executed after SQL Injection treatment
+            $stmt = $conection->prepare($query);
+
+            //Analyzing if thereis SQL Injection
+            //bindValue(':bind_variable', sql_statement, data_type-optional)
+            $stmt->bindValue(':email', $_POST['email']);
+            $stmt->bindValue(':password', $_POST['password'], PDO::PARAM_INT);
+
+            //Execute the query afterSQL Injection treatment
+            $stmt->execute();
+
+            //Fetching the data
+            $user = $stmt->fetch();
+
+            print_r($user);
 
         } catch(PDOException $e) {
             echo 'Erro: '.$e->getCode();
