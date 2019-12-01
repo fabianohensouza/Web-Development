7624bf2e9777a311cff00e1ec54a2acea9dc7585
Fabiano Souza <fabianohensouza@gmail.com>
2019-12-01 12:28:21 -0300

    Finishing the class rout abstraction realized at: Sun Dec

--- 10 - PHP_MVC/miniframework/App/Route.php
+++ 10 - PHP_MVC/miniframework/App/Route.php
@@ -2,28 +2,11 @@
 
 	namespace App;
 
-	class Route {
+	use MF\Init\Bootstrap;
 
-		private $routes;
-		private $valida = False;
+	class Route extends Bootstrap {
 
-		public function __construct() {
-			
-			$this->initRoutes();
-			$this->run($this->getUrl());
-		}
-
-		public function getRoutes() {
-
-			return $this->routes;
-		}
-
-		public function setRoutes(array $routes) {
-
-			$this->routes = $routes;
-		}
-
-		public function initRoutes() {
+		protected function initRoutes() {
 
 			$routes['home'] = array(
 				'route' => '/',
@@ -38,34 +21,6 @@
 			);
 
 			$this->setRoutes($routes);
-		}
-
-		public function getUrl() {
-			return parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-			//return parse_url('www.google.com?x=10');
-		}
-
-		public function run($url) {
-
-			foreach ($this->getRoutes() as $key => $route) {
-
-				if($url ==$route['route']) {
-
-					$class = "App\\Controllers\\".ucfirst($route['controller']);
-
-					$controller = new $class;
-					print_r($controller);
-					$action = $route['action'];
-					echo '<hr>'.$action.'//'.$url.'<hr>';
-					$controller->$action();
-
-					$this->valida = True;
-				}
-			}
-
-			if(!$this->valida) {
-				echo '<hr>A URL '.$url.' Inválida!';
-			}
 		}
 	}
 
---
+++ 10 - PHP_MVC/miniframework/vendor/MF/Init/Bootstrap.php
@@ -0,0 +1,58 @@
+<?php
+
+	namespace MF\Init;
+
+	abstract class Bootstrap {
+
+		private $routes;
+		private $valida = False;
+
+		abstract protected function initRoutes();
+
+		public function __construct() {
+			
+			$this->initRoutes();
+			$this->run($this->getUrl());
+		}
+
+		public function getRoutes() {
+
+			return $this->routes;
+		}
+
+		public function setRoutes(array $routes) {
+
+			$this->routes = $routes;
+		}
+
+		protected function getUrl() {
+
+			return parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+			//return parse_url('www.google.com?x=10');
+		}
+
+		protected function run($url) {
+
+			foreach ($this->getRoutes() as $key => $route) {
+
+				if($url ==$route['route']) {
+
+					$class = "App\\Controllers\\".ucfirst($route['controller']);
+
+					$controller = new $class;
+					print_r($controller);
+					$action = $route['action'];
+					echo '<hr>'.$action.'//'.$url.'<hr>';
+					$controller->$action();
+
+					$this->valida = True;
+				}
+			}
+
+			if(!$this->valida) {
+				echo '<hr>A URL '.$url.' Inválida!';
+			}
+		}
+	}
+
+?>
