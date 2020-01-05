148ca0752145c7f5ebb8b5b8599d5f10cd673704
Fabiano Souza <fabianohensouza@gmail.com>
2020-01-05 12:28:31 -0300

    Creating the register code realized at: Sun Jan

--- 10 - PHP_MVC/Twitter_clone/App/Controllers/IndexController.php
+++ 10 - PHP_MVC/Twitter_clone/App/Controllers/IndexController.php
@@ -18,6 +18,23 @@ class IndexController extends Action {
 		$this->render('inscreverse');
 	}
 
+	public function registrar() {
+
+		echo '<pre>';
+		print_r($_POST);
+		echo '</pre>';
+
+		$usuario = Container::getModel('Usuario');
+
+		$usuario->__set('nome', $_POST['nome']);
+		$usuario->__set('email', $_POST['email']);
+		$usuario->__set('senha', $_POST['senha']);
+
+		echo '<pre>';
+		print_r($usuario);
+		echo '</pre>';
+	}
+
 }
 
 
