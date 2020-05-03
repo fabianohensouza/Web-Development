daa1b409a966050a5d7d8769008dffe984e53d42
Fabiano Souza <fabianohensouza@gmail.com>
2020-05-03 12:16:42 -0300

    realized at: Sun May

--- 11 - API_SLIM-FRAMEWORK/slim-api/src/routes/products.php
+++ 11 - API_SLIM-FRAMEWORK/slim-api/src/routes/products.php
@@ -6,6 +6,7 @@ use App\Models\Product;
 
 $app->group('/api/v1', function() {
 
+  // List all Products
   $this->get('/products/list', function($request, $response) {
 
       $products = Product::get();
@@ -13,10 +14,40 @@ $app->group('/api/v1', function() {
 
   });
 
+  // List a given Product by ID
+  $this->get('/products/list/{id}', function($request, $response, $args) {
+  	
+      $products = Product::findOrFail($args['id']);
+      return $response->withJson($products);
+
+  });
+
+  // Add Products
   $this->post('/products/add', function($request, $response) {
 
       $data = $request->getParsedBody();
-      $products = Product::create();
+      $products = Product::create($data);
+
+      return $response->withJson($products);
+
+  });
+
+  // Update Products by ID
+  $this->put('/products/update/{id}', function($request, $response, $args) {
+
+  	  $data = $request->getParsedBody();
+      $products = Product::findOrFail($args['id']);
+  	  $products->update($data);
+
+      return $response->withJson($products);
+
+  });
+
+  // Delete Products by ID
+  $this->delete('/products/delete/{id}', function($request, $response, $args) {
+
+  	  $products = Product::findOrFail($args['id']);
+  	  $products->delete();
 
       return $response->withJson($products);
 
