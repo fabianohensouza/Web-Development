c0e26a94b01cb82965ea71fa2a5bd05d63c6660a
Fabiano Souza <fabianohensouza@gmail.com>
2020-06-07 12:28:42 -0300

    Creating the ajax requests realized at: Sun Jun

--- 13 - JQUERY/app_dashboard/script.js
+++ 13 - JQUERY/app_dashboard/script.js
@@ -1,3 +1,37 @@
 $(document).ready(() => {
 	
+	$('#documentacao').on('click', () => {
+		$('#pagina').load('documentacao.html')
+	})
+	
+	$('#suporte').on('click', () => {
+		
+		$.get('suporte.html', data => {
+			$('#pagina').html(data)
+		})
+		
+	})
+	
+	$('#sobrenos').on('click', () => {
+		
+		$.post('sobrenos.html', data => {
+			$('#pagina').html(data)
+		})
+	})
+
+	//Ajax Method
+	$('#competencia').on('change', e => {
+
+		let competencia = $(e.target).val()
+
+		//Ajax Method parameters - Method/URL/Data/Success/Error
+		$.ajax({
+			type: 'GET',
+			url: 'app.php',
+			data: `competencia=${competencia}`, //x-www-form-urlencoded - name1=value1&name2=value2&name3=value3
+			success: data => { console.log(data)},
+			error: error => { console.log(error)}
+		})
+
+	})
 })
